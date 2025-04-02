e27ebd7b558fc06a75353baa0d52d6d1e497adbe
Anthony Marinov <anthonymmarinov@gmail.com>
2025-04-01 21:39:54 -0700

    feature: sqlite database connected

--- src/main/java/testapp/controllers/DemoController.java
+++ src/main/java/testapp/controllers/DemoController.java
@@ -1,12 +1,19 @@
 package testapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import testapp.daos.DemoDao;
+
 @Controller
 public class DemoController {
+
+    @Autowired
+    private DemoDao demoDao;
     
     @RequestMapping(value="/index.html", method=RequestMethod.GET)
     @ResponseBody
@@ -15,7 +22,8 @@ public class DemoController {
     }
 
     @RequestMapping(value="/home.html", method=RequestMethod.GET)
-    public String home() {
+    public String home(ModelMap modelMap) {
+        modelMap.addAttribute("name", demoDao.getFirstName());
         return "home.html";
     }
 }
---
+++ src/main/java/testapp/daos/DemoDao.java
@@ -0,0 +1,30 @@
+package testapp.daos;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+
+@Repository(value="testapp.daos.DemoDao")
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+public class DemoDao {
+    
+    private static final String GET_FIRST_NAME = 
+            "SELECT name FROM testtable WHERE idTestTable = 1";
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    public String getFirstName() {
+        List<String> namesFound = jdbcTemplate.queryForList(
+                GET_FIRST_NAME, 
+                new MapSqlParameterSource(), 
+                String.class);
+        return namesFound.get(0);
+    }
+}
